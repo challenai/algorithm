9a3df844cb3121704d98e6efaa40dc5d20231705
ChallenAi <jianglun465@sina.com>
2020-09-30 15:18:31 +0800

    failed 068

--- lc068/solution.go
+++ lc068/solution.go
@@ -1,1 +1,51 @@
 package solution
+
+func fullJustify(words []string, maxWidth int) []string {
+	resu := []string{}
+	current := []string{words[0]}
+	currentLen := len(words[0])
+	for i := 0; i < len(words); i++ {
+		if (currentLen + 1 + len(words[i])) <= maxWidth {
+			current = append(current, words[i])
+			currentLen += len(words[i]) + 1
+		} else {
+			// for j := 0; j < len(current); j++ {
+			// 	print(current[j], " ")
+			// }
+			// println()
+			// clear current
+			resu = append(resu, "")
+			currentLen = maxWidth - currentLen
+			if len(current) == 1 {
+				resu[len(resu)-1] = current[i] + trimSpace(currentLen)
+			} else {
+				trimCnt := currentLen / (len(current) - 1)
+				currentLen %= len(current) - 1
+				for j := 0; j < len(current); j++ {
+					if currentLen > 0 {
+						resu[len(resu)-1] += current[j] + trimSpace(trimCnt+1)
+						currentLen--
+					} else if i == len(resu)-1 {
+						resu[len(resu)-1] += current[j]
+					} else {
+						resu[len(resu)-1] += current[j] + trimSpace(trimCnt)
+					}
+				}
+			}
+			current = []string{words[i]}
+			currentLen = len(words[i])
+		}
+	}
+	for i := 0; i < len(resu); i++ {
+		println(resu[i])
+	}
+	return resu
+}
+
+func trimSpace(n int) string {
+	resu := ""
+	for i := 0; i < n; i++ {
+		resu += " "
+	}
+	return resu
+}
--- lc068/solution_test.go
+++ lc068/solution_test.go
@@ -4,9 +4,37 @@ import (
 	"testing"
 )
 
-func TestX(t *testing.T) {
+func TestFullJustify(t *testing.T) {
 
-	if false {
+	words1 := []string{"This", "is", "an", "example", "of", "text", "justification."}
+	maxWidth1 := 16
+	r1 := []string{
+		"This    is    an",
+		"example  of text",
+		"justification.  ",
+	}
+	words2 := []string{"What", "must", "be", "acknowledgment", "shall", "be"}
+	maxWidth2 := 16
+	r2 := []string{
+		"What   must   be",
+		"acknowledgment  ",
+		"shall be        ",
+	}
+	words3 := []string{"Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"}
+	maxWidth3 := 20
+	r3 := []string{
+		"Science  is  what we",
+		"understand      well",
+		"enough to explain to",
+		"a  computer.  Art is",
+		"everything  else  we",
+		"do                  ",
+	}
+	resu1 := fullJustify(words1, maxWidth1)
+	resu2 := fullJustify(words2, maxWidth2)
+	resu3 := fullJustify(words3, maxWidth3)
+
+	if len(resu1) != len(r1) || len(resu2) != len(r2) || len(resu3) != len(r3) {
 		t.Fail()
 	}
 }
