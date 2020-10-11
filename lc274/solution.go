26d4680e07a7c2427d12b8a9457be54f3ee78372
ChallenAi <jianglun465@sina.com>
2020-10-11 16:08:43 +0800

    AC 274

--- lc274/solution.go
+++ lc274/solution.go
@@ -1,1 +1,19 @@
 package solution
+
+import "sort"
+
+func hIndex(nums []int) int {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] > nums[j]
+	})
+	i := 0
+	for ; i < len(nums); i++ {
+		if nums[i] < i+1 {
+			break
+		}
+	}
+	if nums[len(nums)-1] >= i+1 {
+		i++
+	}
+	return i
+}
