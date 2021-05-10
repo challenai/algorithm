f36a8451803f332b91b233b2c6741118941aa351
ChallenAi <jianglun465@sina.com>
2021-05-10 17:10:50 +0800

    AC 239, descending queue

--- lc239/solution.go
+++ lc239/solution.go
@@ -1,27 +1,29 @@
 package solution
 
 func maxSlidingWindow(nums []int, k int) []int {
-	if k == 1 {
-		return nums
-	}
-	if len(nums) == 0 {
-		return nums
-	}
-	// idea: use a queue to store the max data
-	wndw := []int{nums[0]}
-	resu := []int{}
-	for i := 1; i < len(nums); i++ {
-		if nums[i] < wndw[0] {
-			wndw = append(wndw, nums[i])
-			if len(wndw) > k {
-				wndw = wndw[1:]
-			}
-		} else {
-			wndw = []int{nums[i]}
-		}
-		if i >= k-1 {
-			resu = append(resu, wndw[0])
-		}
-	}
-	return resu
+  // keep window asending or desending so that we just need to compare the first element to find maximum or minimum of windows.
+  rsu := []int{}
+  if len(nums) == 0 {
+    return rsu
+  }
+  if k <= 1 {
+    return nums
+  }
+  window := []int{0}
+  for i := 1; i < len(nums); i++ {
+    // remove overflow elements at beginning
+    if window[0] < i - k + 1{
+      window = window[1:]
+    }
+    // remove smaller elements in list so that our window is purely descending.
+    for len(window) > 0 && nums[i] >= nums[window[len(window)-1]] {
+      window = window[:len(window)-1]
+    }
+    window = append(window, i)
+    // if we our index arrived at where we need to push result (at least k elements)
+    if i >= k-1 {
+      rsu = append(rsu, nums[window[0]])
+    }
+  }
+  return rsu
 }
