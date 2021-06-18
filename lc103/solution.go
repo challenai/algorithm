fe06992c0825be8e745e2781dc138547b7c3bd59
ChallenAi <fengyiai@gmail.com>
2021-06-19 01:07:11 +0800

    update 103

--- lc103/solution.go
+++ lc103/solution.go
@@ -6,42 +6,36 @@ type TreeNode struct {
 }
 
 func zigzagLevelOrder(root *TreeNode) [][]int {
-	resu := [][]int{{}}
-	var ptr *TreeNode
-	reverse := true
-	dq := []*TreeNode{root}
-	counter := 1
-	for len(dq) > 0 {
-		ptr = dq[0]
-		dq = dq[1:]
-		if reverse {
-			if ptr.Right != nil {
-				dq = append(dq, ptr.Right)
-			}
-			if ptr.Left != nil {
-				dq = append(dq, ptr.Left)
-			}
-		} else {
-			if ptr.Left != nil {
-				dq = append(dq, ptr.Left)
-			}
-			if ptr.Right != nil {
-				dq = append(dq, ptr.Right)
-			}
-		}
-		counter--
-		resu[len(resu)-1] = append(resu[len(resu)-1], ptr.Val)
-		if counter == 0 {
-			reverse = !reverse
-			resu = append(resu, []int{})
-			counter = len(dq)
-		}
-	}
-	// for i := 0; i < len(resu); i++ {
-	// 	for j := 0; j < len(resu[i]); j++ {
-	// 		print(resu[i][j], " ")
-	// 	}
-	// 	println()
-	// }
-	return resu
+  if root == nil {
+    return [][]int{}
+  }
+  // 3
+  // 9 20 -> 20 9
+  // 1 1 15 7
+  rsu := [][]int{{}}
+  q := []*TreeNode{root}
+  sz := 1
+  reverse := true
+  for len(q) > 0 {
+    sz--;
+    temp := q[0];
+    q.unshift();
+    if temp.Left != nil {
+      q = append(q, temp.Left)
+    }
+    if temp.Right != nil {
+      q = append(q, temp.Right)
+    }
+    if reverse {
+      rsu[len(rsu)-1] = append(rsu[len(rsu)-1], temp.Val)
+    } else {
+      rsu[len(rsu)-1] = append([]int{temp.Val}, rsu[len(rsu)-1]...)
+    }
+    if sz == 0 && len(q) > 0 {
+      reverse = !reverse
+      rsu = append(rsu, []int{})
+      sz = len(q)
+    }
+  }
+  return rsu
 }
